51e5a4812b6d5436b875f06ecdeeca333a2542ad
Alex <chaza.mckay@me.com>
2022-06-08 00:01:41 +0930

    NEw try 300000000

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/unknown', {
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/unknown', {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// });
+// TEMP TRY FIX
+mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
--- server/server.js
+++ server/server.js
@@ -17,17 +17,21 @@ const server = new ApolloServer({
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-  app.get("*", (req, res) => {
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '../client/build')));
+//   app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../client/build/index.html"));
+// });
+// }
+
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
+// });
+// TEMP TRY FIX
+app.use(express.static(path.join(__dirname, '../client/build')));
+app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../client/build/index.html"));
-  });
-}
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
-
 
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
